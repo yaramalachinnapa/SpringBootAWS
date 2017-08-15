ce4af2ea73fe2c4e7d187724a29b9504a91203d3
Chinnapa Reddy <yaramalachinnapa@gmail.com>
2017-08-15 16:01:52 -0500

    Initial commit

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,16 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @RequestMapping("/greeting")
+    public String greeting()  {
+        return "hello World";
+
+    }
+}
